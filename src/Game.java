33089218210ffa2d21680021a99f6c23089e36fb
alexstoffel <alex.stoffel@menloschool.org>
2024-11-17 10:53:34 -0800

    I started all the classes, not sure if it works yet

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Game {
+    private int round;
+    private ArrayList<Player> players;
+
+    // Constructor for game
+    public Game(){
+        players = new ArrayList<Player>();
+    }
+    // Can add a player to the game
+    public void add(Player player){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+    private int points;
+    private String name;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
